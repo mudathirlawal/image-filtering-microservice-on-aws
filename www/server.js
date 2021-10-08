0977f7c1ced16ce3cb44bcafbaadc05060ce8dc5
Mudathir Lawal <nigercode@gmail.com>
2021-10-08 11:14:14 +0100

    feat: Add transpiled code

---
+++ www/server.js
@@ -0,0 +1,43 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const util_1 = require("./util/util");
+(() => __awaiter(this, void 0, void 0, function* () {
+    // Init the Express application
+    const app = express_1.default();
+    // Set the network port
+    const port = process.env.PORT || 8082;
+    // Use the body parser middleware for post requests
+    app.use(body_parser_1.default.json());
+    // Implement a RESTful endpoint
+    app.get("/filteredimage/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        let { image_url } = req.query;
+        if (!image_url) {
+            return res.status(400).send(`image_url is required`);
+        }
+        const filtered_image_path = yield util_1.filterImageFromURL(image_url);
+        const filtered_image_paths = [filtered_image_path];
+        return res.status(200).sendFile(filtered_image_path, () => __awaiter(this, void 0, void 0, function* () { return yield util_1.deleteLocalFiles(filtered_image_paths); }));
+    }));
+    app.get("/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        res.send("try GET /filteredimage?image_url={{}}");
+    }));
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running http://localhost:${port}`);
+        console.log(`press CTRL+C to stop server`);
+    });
+}))();
+//# sourceMappingURL=server.js.map
